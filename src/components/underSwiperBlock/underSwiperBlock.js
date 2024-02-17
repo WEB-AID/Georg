3638c051c3033a3779acfd8514f9ccfefffcb9e3
WEB-AID <web.aid.dev@gmail.com>
2024-02-17 17:22:42 +0100

    11 commit

--- src/components/underSwiperBlock/underSwiperBlock.js
+++ src/components/underSwiperBlock/underSwiperBlock.js
@@ -10,6 +10,10 @@ export default class UnderSwiperBlock extends React.Component {
         return(
             <div className="aboutUsBlock">
                 <div className="aboutUsBlockWrapper">
+                    <div className="text1">Качество выпускаемой<br></br>продукции сертифицированно<br></br> ISO 22000 и является лидером<br></br>на Грузинском рынке</div>
+                    <div className="text2">Корм для поголовья<br></br>изготавливается исключительно<br></br>из органических компонентов</div>
+                    <div className="text3">Процессы внутри компании<br></br>построены таким образом<br></br>что бы развитие происходило<br></br>ежедневно 24 на 7</div>
+                    <div className="text4">Генетический фонд нашего<br></br>поголовья берет своё начало<br></br>в Дании и является<br></br>лидером свиноводства</div>
                     {/* <img className="photo1" src={photo} alt=""></img>
                     <img className="photo2" src={photo} alt=""></img>
                     <img className="photo3" src={photo} alt=""></img>
