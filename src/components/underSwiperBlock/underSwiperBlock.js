1989c21153b1ab021ad6e59072ea474adc644a0b
WEB-AID <web.aid.dev@gmail.com>
2024-02-19 16:35:07 +0100

    13 commit

--- src/components/underSwiperBlock/underSwiperBlock.js
+++ src/components/underSwiperBlock/underSwiperBlock.js
@@ -9,10 +9,26 @@ export default class UnderSwiperBlock extends React.Component {
         return(
             <div className="aboutUsBlock">
                 <div className="aboutUsBlockWrapper">
-                    <div className="text1"><span>Качество выпускаемой<br></br>продукции сертифицированно<br></br> ISO 22000 и является лидером<br></br>на Грузинском рынке</span></div>
-                    <div className="text2">Корм для поголовья<br></br>изготавливается исключительно<br></br>из органических компонентов</div>
-                    <div className="text3">Процессы внутри компании<br></br>построены таким образом<br></br>что бы развитие происходило<br></br>ежедневно 24 на 7</div>
-                    <div className="text4">Генетический фонд нашего<br></br>поголовья берет своё начало<br></br>в Дании и является<br></br>лидером свиноводства</div>
+                    <div className="text1">
+                        <div className="text1_1">
+                            Качество выпускаемой<br></br>продукции сертифицированно<br></br> ISO 22000 и является лидером<br></br>на Грузинском рынке
+                        </div>
+                    </div>
+                    <div className="text2">
+                        <div className="text2_1">
+                            Корм для поголовья<br></br>изготавливается исключительно<br></br>из органических компонентов
+                        </div>
+                    </div>
+                    <div className="text3">
+                        <div className="text3_1">
+                            Процессы внутри компании<br></br>построены таким образом<br></br>что бы развитие происходило<br></br>ежедневно 24 на 7
+                        </div>
+                    </div>
+                    <div className="text4"> 
+                        <div className="text4_1">
+                            Генетический фонд нашего<br></br>поголовья берет своё начало<br></br>в Дании и является<br></br>лидером свиноводства
+                        </div>
+                    </div>
                 </div>
             </div>
         )
